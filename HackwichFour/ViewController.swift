ccdf73d150689d8531d13c9e30247eb3ab32fc37
CM Student <cmstudent@uhwo-acm-10.local>
2019-02-28 09:39:02 -1000

    Initial Commit

---
+++ HackwichFour/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HackwichFour
+//
+//  Created by CM Student on 2/28/19.
+//  Copyright © 2019 CM Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ HackwichFourTests/HackwichFourTests.swift
@@ -0,0 +1,34 @@
+//
+//  HackwichFourTests.swift
+//  HackwichFourTests
+//
+//  Created by CM Student on 2/28/19.
+//  Copyright © 2019 CM Student. All rights reserved.
+//
+
+import XCTest
+@testable import HackwichFour
+
+class HackwichFourTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
